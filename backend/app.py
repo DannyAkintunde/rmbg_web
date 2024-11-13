ed4042ea9731448c5cc6452875b434c9a302bb60
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-11-13 02:04:19 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ models_path.mkdir(parents=True, exist_ok=True)
 models = ["birefnet-cod", "birefnet-dis", "birefnet-general", "birefnet-general-lite", "birefnet-hrsod", "birefnet-massive", "birefnet-portrait", "isnet-anime", "isnet-general-use", "sam", "silueta", "u2net", "u2net_cloth_seg", "u2net_human_seg", "u2netp"]
 # models = [file.name for file in models_path.iterdir() if file.is_file() and file.suffix == '.onnx']
 
-session = new_session(getenv('DEFAULT_MODEL, 'u2netp'))
+session = new_session(getenv('DEFAULT_MODEL', 'u2netp'))
 
 def hex_to_rgba(hex_code):
     hex_code = hex_code.lstrip('#')
